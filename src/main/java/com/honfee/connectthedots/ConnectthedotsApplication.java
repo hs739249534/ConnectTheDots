c0b2dfbaad8982ef01c66e3e3422716c308bd010
hs739249534 <hs739249534@163.com>
2023-04-02 23:55:39 +0800

    2023-04-02 add 初始化

---
+++ src/main/java/com/honfee/connectthedots/ConnectthedotsApplication.java
@@ -0,0 +1,13 @@
+package com.honfee.connectthedots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectthedotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectthedotsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/honfee/connectthedots/ConnectthedotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honfee.connectthedots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectthedotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
